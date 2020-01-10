1272dd2b6bcc435f9b7f8a645f0ba0309d0335a8
macgibbons <macgibbons@gmail.com>
2020-01-10 15:30:41 -0600

    styled the politician cards to be easier to read

--- scripts/politicians/politician.js
+++ scripts/politicians/politician.js
@@ -1,12 +1,22 @@
 const Politician = (politician) => {
     return `
-    <section class="politician__card">
-    <div>${politician.name.first} ${politician.name.last}</div>
-    <div class="district">${politician.district}</div>
-    <div class="phone">${politician.phone}</div>
-    </section>
+    <article class="politician__card">
+        <section class="politician">
+            <header class="politician__name">
+                <h1>${politician.name.first} ${politician.name.last}</h1>
+            </header>
+
+            <section class="politician__info">
+                <div>Age: ${politician.age}</div>
+                <div class="district">${politician.district}</div>
+                <div class="phone">${politician.phone}</div>
+            </section>
+        </section>
+    </article>
     `
 
 }
 
 export default Politician
+
+
